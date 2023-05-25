e46cf58e9f0237265fe7713976ea51325543a75c
Daniel <satelerd@gmail.com>
2023-05-25 17:07:43 -0400

    add data

--- main.py
+++ main.py
@@ -1,33 +1,37 @@
 import csv
+import umap
 import seaborn as sns
 from scipy import spatial
+import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
 from sentence_transformers import SentenceTransformer
-import matplotlib.pyplot as plt
 
-# Read attendees and their responses from a CSV file, replace attendees.csv with own link or file name
-attendees_map = {}
-with open('attendees.csv', newline='') as csvfile:
-    attendees = csv.reader(csvfile, delimiter=',', quotechar='"')
-    next(attendees)  # Skip the header row
-    for row in attendees:
+
+# Read data and their responses from a CSV file, replace data.csv with own link or file name
+data_map = {}
+with open('data.csv', newline='') as csvfile:
+    data = csv.reader(csvfile, delimiter=',', quotechar='"')
+    next(data)  # Skip the header row
+    for row in data:
         name, paragraph = row
-        attendees_map[paragraph] = name
+        data_map[paragraph] = name
+print("Data loaded successfully.")
 
 # Generate sentence embeddings
 model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
-paragraphs = list(attendees_map.keys())
+paragraphs = list(data_map.keys())
 embeddings = model.encode(paragraphs)
+print("Embeddings generated successfully.")
     
 # Create a dictionary to store embeddings for each person
-person_embeddings = {attendees_map[paragraph]: embedding for paragraph, embedding in zip(paragraphs, embeddings)}
+person_embeddings = {data_map[paragraph]: embedding for paragraph, embedding in zip(paragraphs, embeddings)}
 
 # Reducing dimensionality of embedding data, scaling to coordinate domain/range
-import umap
 reducer = umap.UMAP()
 scaler = StandardScaler()
 scaled_data = scaler.fit_transform(list(person_embeddings.values()))
 reduced_data = reducer.fit_transform(scaled_data)
+print("Dimensionality reduction and scaling completed successfully.")
 
 # Creating lists of coordinates with accompanying labels
 x = [row[0] for row in reduced_data]
@@ -37,8 +41,9 @@ label = list(person_embeddings.keys())
 # Plotting and annotating data points
 plt.scatter(x,y)
 for i, name in enumerate(label):
-    plt.annotate(name, (x[i], y[i]), fontsize="12", color="white")  # Increased font size to 12 and set color to white
+    plt.annotate(name, (x[i], y[i]), fontsize="15", color="black")  # Increased font size to 12 and set color to white
 
 # Clean-up and Export
 plt.axis('off')
 plt.savefig('visualization.png', dpi=800)
+print("Visualization saved successfully.")
